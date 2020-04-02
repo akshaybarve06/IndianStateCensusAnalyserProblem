619a43e9e6c54dc3cb95497e8eede6b279cd1815
Akshay Barve <akshaybarve06@gmail.com>
2020-04-02 11:08:37 +0530

    Added: TC 1.2 If File CSV is Incorrect Return Custom Exception

---
+++ src/main/java/com/exception/CensusAnalyserCustomException.java
@@ -0,0 +1,12 @@
+package com.exception;
+
+public class CensusAnalyserCustomException extends Exception{
+    public enum TypeOfExceptionThrown{
+        FILE_NOT_FOUND_EXCEPTION
+    }
+    public TypeOfExceptionThrown typeOfException;
+
+    public CensusAnalyserCustomException(TypeOfExceptionThrown typeOfException){
+        this.typeOfException = typeOfException;
+    }
+}
--- src/test/java/StateCensusTest.java
+++ src/test/java/StateCensusTest.java
@@ -1,3 +1,4 @@
+import com.exception.CensusAnalyserCustomException;
 import com.services.StateCensusAnalyser;
 
 import org.junit.Assert;
@@ -5,10 +6,21 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusTest {
-        @Test
-        public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws IOException{
-            StateCensusAnalyser censusAnalyser=new StateCensusAnalyser();
+    private static final String INPUT_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
+
+    @Test
+        public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws IOException, CensusAnalyserCustomException {
+            StateCensusAnalyser censusAnalyser=new StateCensusAnalyser(INPUT_CSV_FILE_PATH);
             int noOfRecords=censusAnalyser.loadData();
             Assert.assertEquals(29,noOfRecords);
+        }
+        @Test
+        public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() throws IOException{
+            try {
+                StateCensusAnalyser censusAnalyser = new StateCensusAnalyser(INPUT_CSV_FILE_PATH);
+                censusAnalyser.loadData();
+            } catch (CensusAnalyserCustomException e) {
+                Assert.assertEquals(CensusAnalyserCustomException.TypeOfExceptionThrown.FILE_NOT_FOUND_EXCEPTION,e.typeOfException);
+            }
         }
 }
