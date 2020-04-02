7175be24fb12ec25155c704dd574fca7653d8fac
Akshay Barve <akshaybarve06@gmail.com>
2020-04-02 12:04:52 +0530

    Added: TC 1.3 If File Type is Incorrect Return Custom Exception

--- src/test/java/StateCensusTest.java
+++ src/test/java/StateCensusTest.java
@@ -6,21 +6,23 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusTest {
-    private static final String INPUT_CSV_FILE_PATH = "./src/test/resources/StateCensusData.csv";
+    private static final String INPUT_CSV_FILE_PATH = "./src/test/resources/StateCensusData.jpg";
 
     @Test
-        public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws IOException, CensusAnalyserCustomException {
-            StateCensusAnalyser censusAnalyser=new StateCensusAnalyser(INPUT_CSV_FILE_PATH);
-            int noOfRecords=censusAnalyser.loadData();
-            Assert.assertEquals(29,noOfRecords);
+    public void givenStateCensusCSV_WhenConditionTrue_ReturnNumberOfRecordMatch() throws IOException, CensusAnalyserCustomException {
+        StateCensusAnalyser censusAnalyser=new StateCensusAnalyser(INPUT_CSV_FILE_PATH);
+        int noOfRecords=censusAnalyser.loadData();
+        Assert.assertEquals(29,noOfRecords);
+    }
+    @Test
+    public void givenStateCensusCSV_WhenImproperFileName_ReturnsException() throws IOException{
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser(INPUT_CSV_FILE_PATH);
+            censusAnalyser.loadData();
+        } catch (CensusAnalyserCustomException e) {
+            Assert.assertEquals(CensusAnalyserCustomException.TypeOfExceptionThrown.FILE_NOT_FOUND_EXCEPTION,e.typeOfException);
+            System.out.println("Wrong File Given");
         }
-        @Test
-        public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() throws IOException{
-            try {
-                StateCensusAnalyser censusAnalyser = new StateCensusAnalyser(INPUT_CSV_FILE_PATH);
-                censusAnalyser.loadData();
-            } catch (CensusAnalyserCustomException e) {
-                Assert.assertEquals(CensusAnalyserCustomException.TypeOfExceptionThrown.FILE_NOT_FOUND_EXCEPTION,e.typeOfException);
-            }
-        }
+    }
+
 }
